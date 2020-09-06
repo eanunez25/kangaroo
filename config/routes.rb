1b1f6519073c28512ef9642a10afe1ed5e69c365
Elvin Nunez <eanunez25@gmail.com>
2020-09-06 00:54:35 -0500

    style navbar and add links

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,14 @@
 Rails.application.routes.draw do
   devise_for :users
+  resources :static_pages
+
+  root 'static_pages#home'
+  
   
   devise_scope :user do
     get 'signup', to: 'devise/registrations#new'
-    get 'signin', to: 'devise/sessions#new'
+    get 'login', to: 'devise/sessions#new'
+    delete 'logout', to: 'devise/sessions#destroy'
   end
 
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_09_05_063029) do
+ActiveRecord::Schema.define(version: 2020_09_06_055148) do
 
   create_table "users", force: :cascade do |t|
     t.string "username"
@@ -18,6 +18,7 @@ ActiveRecord::Schema.define(version: 2020_09_05_063029) do
     t.string "encrypted_password", default: "", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.datetime "remember_created_at"
     t.index "\"reset_password_token\"", name: "index_users_on_reset_password_token", unique: true
     t.index ["email"], name: "index_users_on_email", unique: true
   end
