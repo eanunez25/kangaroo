1264a5469527cc91af9278cda6966403047a3359
Elvin Nunez <eanunez25@gmail.com>
2020-09-06 21:58:08 -0500

    style flash messages

--- config/routes.rb
+++ config/routes.rb
@@ -7,7 +7,9 @@ Rails.application.routes.draw do
   
   
   devise_scope :user do
+    # register
     get 'signup', to: 'devise/registrations#new'
+    # sessions
     get 'login', to: 'devise/sessions#new'
     delete 'logout', to: 'devise/sessions#destroy'
   end
